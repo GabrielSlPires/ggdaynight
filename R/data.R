ac0bb116e6726fae4d891a8457c5c6fdfa314fda
Gabriel S. Pires <gabriel.slpires@gmail.com>
2024-05-18 14:41:51 -0300

    add example data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Sensor Temperature Data
+#'
+#' A dataset containing temperature readings from multiple sensors over a specified period.
+#'
+#' @format ## `daynight_temperature`
+#' A data frame with 1833 rows and 3 columns:
+#' \describe{
+#'   \item{sensor}{Sensor identifier (e.g., A, B)}
+#'   \item{datetime}{Timestamp of the reading (POSIXct format)}
+#'   \item{temperature}{Temperature reading in degrees Celsius}
+#' }
+#' @source Sensor readings collected on 2024-04-23 in Campinas-SP, Brazil.
+"daynight_temperature"
